41333d48a0be11a42c6e7cd46a0837f6b8868640
Nícolas (paratudo) <nicolassilva0109@gmail.com>
2023-05-29 11:19:02 -0300

    separated draw functions into their own object
    Signed-off-by: Nícolas (paratudo) <nicolassilva0109@gmail.com>

--- rooms.js
+++ rooms.js
@@ -9,15 +9,13 @@ let field = {
 	 * The number of columns in the field.
 	 * @type {number}
 	 */
-	columns: 30,
+	columns: 40,
 
 	/**
 	 * The number of rows in the field.
 	 * @type {number}
 	 */
-	rows: 17,
-
-	class cellDataBlueprint :
+	rows: 40,
 
 	/**
 	 * Initializes the game field by populating it with cells.
@@ -89,141 +87,162 @@ let field = {
 		// not really a dirty hack but still annoys me
 		return cell;
 	},
-
-	draw: {
-		/**
-		 * Draws a line using a simple algorithm.
-		 *
-		 * @param {string} char - The character used to draw the line.
-		 * @param {Object} startPoint - The starting point of the line.
-		 * @param {number} startPoint.x - The x-coordinate of the starting point.
-		 * @param {number} startPoint.y - The y-coordinate of the starting point.
-		 * @param {Object} endPoint - The ending point of the line.
-		 * @param {number} endPoint.x - The x-coordinate of the ending point.
-		 * @param {number} endPoint.y - The y-coordinate of the ending point.
-		 *
-		 * @description
-		 * This function draws a line between the given start and end points using a simple algorithm.
-		 * The line can be drawn vertically (along the y-axis) or horizontally (along the x-axis).
-		 * If the x-coordinates of the start and end points are the same, the line is drawn along the y-axis.
-		 * If the y-coordinates are the same, the line is drawn along the x-axis.
-		 * The function sets the specified character in each cell along the line path using the `setCellContent` function.
-		 * If the start and end points do not align along any axis, an "Invalid line coordinates" message is logged to the console.
-		 */
-		_simpleLine: function (char, startPoint, endPoint) {
-			// Convert coordinates to integers
-			startPoint.x = Math.floor(startPoint.x);
-			startPoint.y = Math.floor(startPoint.y);
-			endPoint.x = Math.floor(endPoint.x);
-			endPoint.y = Math.floor(endPoint.y);
-
-			if (Math.abs(startPoint.x - endPoint.x) === 0) {
-				// If the x-coordinates are the same, draw along the y-axis
-				let minY = Math.min(startPoint.y, endPoint.y);
-				let maxY = Math.max(startPoint.y, endPoint.y);
-
-				for (let y = minY; y <= maxY; y++) {
-					field.setCellContent(char, startPoint.x, y);
-				}
-			} else if (Math.abs(startPoint.y - endPoint.y) === 0) {
-				// If the y-coordinates are the same, draw along the x-axis
-				let minX = Math.min(startPoint.x, endPoint.x);
-				let maxX = Math.max(startPoint.x, endPoint.x);
-
-				for (let x = minX; x <= maxX; x++) {
-					field.setCellContent(char, x, startPoint.y);
-				}
-			} else {
-				console.log("Invalid line coordinates");
-			}
-		},
-
-		/**
-		 * Draws a line on the field using the Bresenham algorithm.
-		 * @param {string} char - The character used to draw the line.
-		 * @param {Object} startPoint - The starting point of the line.
-		 * @param {number} startPoint.x - The x-coordinate of the starting point.
-		 * @param {number} startPoint.y - The y-coordinate of the starting point.
-		 * @param {Object} endPoint - The ending point of the line.
-		 * @param {number} endPoint.x - The x-coordinate of the ending point.
-		 * @param {number} endPoint.y - The y-coordinate of the ending point.
-		 */
-
-		// TODO?: return all the elements used for the line for setting classes
-		line: function (char, startPoint, endPoint) {
-			const dx = Math.abs(endPoint.x - startPoint.x);
-			const dy = Math.abs(endPoint.y - startPoint.y);
-			const sx = startPoint.x < endPoint.x ? 1 : -1;
-			const sy = startPoint.y < endPoint.y ? 1 : -1;
-			let err = dx - dy;
-			let x = startPoint.x;
-			let y = startPoint.y;
-
-			while (x !== endPoint.x || y !== endPoint.y) {
-				field.setCellContent(char, x, y);
-				const err2 = 2 * err;
-				if (err2 > -dy) {
-					err -= dy;
-					x += sx;
-				}
-				if (err2 < dx) {
-					err += dx;
-					y += sy;
-				}
-			}
-			// Set the content for the end point
-			field.setCellContent(char, endPoint.x, endPoint.y);
-		},
-
-		/**
-		 * Draws a square on the field using the provided character.
-		 * @param {string} char - The character used to draw the square.
-		 * @param {Object} startPoint - The top-left point of the square.
-		 * @param {number} startPoint.x - The x-coordinate of the top-left point.
-		 * @param {number} startPoint.y - The y-coordinate of the top-left point.
-		 * @param {Object} endPoint - The bottom-right point of the square.
-		 * @param {number} endPoint.x - The x-coordinate of the bottom-right point.
-		 * @param {number} endPoint.y - The y-coordinate of the bottom-right point.
-		 */
-		square: function (char, startPoint, endPoint) {
-			// Make the startPoint be the top left and endPoint be the bottom right
-			if (endPoint.x < startPoint.x) {
-				[startPoint.x, endPoint.x] = [endPoint.x, startPoint.x]; // Switch them around
-			}
-
-			if (endPoint.y < startPoint.y) {
-				[startPoint.y, endPoint.y] = [endPoint.y, startPoint.y]; // Switch them around
-			}
-
-			/**
-			 * Epic square reference
-			 *    startPoint.x, startPoint.y___startPoint.x, endPoint.y
-			 *    		|							|
-			 *    	   	|							|
-			 *    		|							|
-			 *    endPoint.x, startPoint.y___endPoint.x, endPoint.y
-			 */
-
-			// Create coordinates for the other two points
-			let topRightPoint = field.getCell({ x: endPoint.x, y: startPoint.y });
-			let bottomLeftPoint = field.getCell({ x: startPoint.x, y: endPoint.y });
-
-			field.draw.line(char, startPoint, topRightPoint); // Top side
-			field.draw.line(char, topRightPoint, endPoint); // Right side
-			field.draw.line(char, endPoint, bottomLeftPoint); // Bottom side
-			field.draw.line(char, bottomLeftPoint, startPoint); // Left side
-		},
-
-		filledBox: function (char, startPoint, endPoint) {
-
-		let inde
-			for (let index.x = 0; index.x < ; index.x++) {
-				const element = array[index.x];
-				
-			}
-		},
-	},
 };
+
+let draw = {
+	/**
+	 * Draws a line using a simple algorithm.
+	 *
+	 * @param {string} char - The character used to draw the line.
+	 * @param {Object} startPoint - The starting point of the line.
+	 * @param {number} startPoint.x - The x-coordinate of the starting point.
+	 * @param {number} startPoint.y - The y-coordinate of the starting point.
+	 * @param {Object} endPoint - The ending point of the line.
+	 * @param {number} endPoint.x - The x-coordinate of the ending point.
+	 * @param {number} endPoint.y - The y-coordinate of the ending point.
+	 *
+	 * @description
+	 * This function draws a line between the given start and end points using a simple algorithm.
+	 * The line can be drawn vertically (along the y-axis) or horizontally (along the x-axis).
+	 * If the x-coordinates of the start and end points are the same, the line is drawn along the y-axis.
+	 * If the y-coordinates are the same, the line is drawn along the x-axis.
+	 * The function sets the specified character in each cell along the line path using the `setCellContent` function.
+	 * If the start and end points do not align along any axis, an "Invalid line coordinates" message is logged to the console.
+	 */
+	_simpleLine: function (char, startPoint, endPoint) {
+		// Convert coordinates to integers
+		startPoint.x = Math.floor(startPoint.x);
+		startPoint.y = Math.floor(startPoint.y);
+		endPoint.x = Math.floor(endPoint.x);
+		endPoint.y = Math.floor(endPoint.y);
+
+		if (Math.abs(startPoint.x - endPoint.x) === 0) {
+			// If the x-coordinates are the same, draw along the y-axis
+			let minY = Math.min(startPoint.y, endPoint.y);
+			let maxY = Math.max(startPoint.y, endPoint.y);
+
+			for (let y = minY; y <= maxY; y++) {
+				field.setCellContent(char, startPoint.x, y);
+			}
+		} else if (Math.abs(startPoint.y - endPoint.y) === 0) {
+			// If the y-coordinates are the same, draw along the x-axis
+			let minX = Math.min(startPoint.x, endPoint.x);
+			let maxX = Math.max(startPoint.x, endPoint.x);
+
+			for (let x = minX; x <= maxX; x++) {
+				field.setCellContent(char, x, startPoint.y);
+			}
+		} else {
+			console.log("Invalid line coordinates");
+		}
+	},
+
+	/**
+	 * Draws a line on the field using the Bresenham algorithm.
+	 * @param {string} char - The character used to draw the line.
+	 * @param {Object} startPoint - The starting point of the line.
+	 * @param {number} startPoint.x - The x-coordinate of the starting point.
+	 * @param {number} startPoint.y - The y-coordinate of the starting point.
+	 * @param {Object} endPoint - The ending point of the line.
+	 * @param {number} endPoint.x - The x-coordinate of the ending point.
+	 * @param {number} endPoint.y - The y-coordinate of the ending point.
+	 */
+
+	// TODO?: return all the elements used for the line for setting classes
+	line: function (char, startPoint, endPoint) {
+		const dx = Math.abs(endPoint.x - startPoint.x);
+		const dy = Math.abs(endPoint.y - startPoint.y);
+		const sx = startPoint.x < endPoint.x ? 1 : -1;
+		const sy = startPoint.y < endPoint.y ? 1 : -1;
+		let err = dx - dy;
+		let x = startPoint.x;
+		let y = startPoint.y;
+
+		while (x !== endPoint.x || y !== endPoint.y) {
+			field.setCellContent(char, x, y);
+			const err2 = 2 * err;
+			if (err2 > -dy) {
+				err -= dy;
+				x += sx;
+			}
+			if (err2 < dx) {
+				err += dx;
+				y += sy;
+			}
+		}
+		// Set the content for the end point
+		field.setCellContent(char, endPoint.x, endPoint.y);
+	},
+
+	/**
+	 * Draws a square on the field using the provided character.
+	 * @param {string} char - The character used to draw the square.
+	 * @param {Object} startPoint - The top-left point of the square.
+	 * @param {number} startPoint.x - The x-coordinate of the top-left point.
+	 * @param {number} startPoint.y - The y-coordinate of the top-left point.
+	 * @param {Object} endPoint - The bottom-right point of the square.
+	 * @param {number} endPoint.x - The x-coordinate of the bottom-right point.
+	 * @param {number} endPoint.y - The y-coordinate of the bottom-right point.
+	 */
+	square: function (char, startPoint, endPoint) {
+		// Make the startPoint be the top left and endPoint be the bottom right
+		if (endPoint.x < startPoint.x) {
+			[startPoint.x, endPoint.x] = [endPoint.x, startPoint.x]; // Switch them around
+		}
+
+		if (endPoint.y < startPoint.y) {
+			[startPoint.y, endPoint.y] = [endPoint.y, startPoint.y]; // Switch them around
+		}
+
+		/**
+		 * Epic square reference
+		 *    startPoint.x, startPoint.y___startPoint.x, endPoint.y
+		 *    		|							|
+		 *    	   	|							|
+		 *    		|							|
+		 *    endPoint.x, startPoint.y___endPoint.x, endPoint.y
+		 */
+
+		// Create coordinates for the other two points
+		let topRightPoint = field.getCell({ x: endPoint.x, y: startPoint.y });
+		let bottomLeftPoint = field.getCell({ x: startPoint.x, y: endPoint.y });
+
+		draw.line(char, startPoint, topRightPoint); // Top side
+		draw.line(char, topRightPoint, endPoint); // Right side
+		draw.line(char, endPoint, bottomLeftPoint); // Bottom side
+		draw.line(char, bottomLeftPoint, startPoint); // Left side
+	},
+
+	//filledBox: function (char, startPoint, endPoint) {
+
+	// let inde
+	// 	for (let index.x = 0; index.x < ; index.x++) {
+	// 		const element = array[index.x];
+
+	// 	}
+	// },
+};
+
+
+/**
+ * Functions to handle messy inputs
+ * 
+ * !(TRY TO USE THEM ONLY) FOR DEBUGGING PURPOSES!
+ */
+let inputSanitizer = {
+
+	/**
+	 * recieves x y and char
+	 * returns object with those properties 
+	 */
+	coordsObjectificator: function () {
+		
+	},
+
+	integer : function () {
+		
+	},
+}
 
 let rootStyle = document.documentElement.style;
 rootStyle.setProperty("--columns", field.columns);
