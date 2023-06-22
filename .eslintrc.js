18344acff45f685c9fec0c21c65a32c3c139b397
Nícolas Sousa <nicolassilva0109@gmail.com>
2023-06-22 11:08:47 -0300

    Added eslint to the project
    Signed-off-by: João Victor <joaobaracui26@gmail.com>
    Signed-off-by: n1colassilva <nicolassilva0109@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "plugin:prettier/recommended"], // Add 'plugin:prettier/recommended' here
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
