dc7da2f8c3295011b6287112af2bb5b3cd4a137b
Nícolas Sousa <nicolassilva0109@gmail.com>
2023-08-02 11:22:48 -0300

    not functional FIX OLD START MENU USING TEMPFIX()
    Signed-off-by: Nícolas Sousa <nicolassilva0109@gmail.com>

--- javascript/editor.js
+++ javascript/editor.js
@@ -153,6 +153,7 @@ editor = {
       }
     }
 
+    // Saving the game grid to include it map package
     const copyMatrix = [];
     const originalMatrix = field.cellMatrix.matrix;
 
@@ -167,17 +168,35 @@ editor = {
       });
     });
 
-    _downloadJSON(copyMatrix);
+    // saving other miscellaneous data
+
+    const collisionGrid = {};
+
+    // Load player data, could include more stuff later on
+    const playerStatus = {
+      movementAllow: player.allowMovement,
+      visibilityAllow: player.show,
+      spawn: null, // TODO: Figure this out
+    };
+
+    // Filling out the save package (there's a better name but this one sounds cooler)
+    const saveData = {
+      matrix: copyMatrix,
+      collisionGrid: collisionGrid,
+      player: playerStatus,
+    };
+    _downloadJSON(saveData);
   },
   load: () => {
-    fetch("Rooms_title_v0.json")
+    fetch("start_menu.json")
       .then((response) => response.json())
       .then((data) => {
         // Here, the 'data' variable contains the parsed JSON object.
         // Now you can use the data to rebuild your map.
         // 2-dimensional array parser 2000
-        for (let row = 0; row < data.length; row++) {
-          for (let column = 0; column < data[row].length; column++) {
+        gameGrid = data.matrix;
+        for (let row = 0; row < gameGrid; row++) {
+          for (let column = 0; column < gameGrid[row].length; column++) {
             const loadedCell = data[row][column];
             const realCell = field.getCell({
               x: loadedCell.x,
@@ -190,5 +209,54 @@ editor = {
       .catch((error) => {
         console.error("Error fetching the JSON file:", error);
       });
+  },
+  tempfix: () => {
+    fetch("start_menu.json")
+      .then((response) => response.json())
+      .then((data) => {
+        /**
+         * @param {*} jsonContent
+         */
+        function _downloadJSON(jsonContent) {
+          const jsonStr = JSON.stringify(jsonContent);
+          const blob = new Blob([jsonStr], { type: "application/json" });
+
+          const downloadLink = document.createElement("a");
+          downloadLink.href = URL.createObjectURL(blob);
+
+          const filename = prompt("Enter a filename for the JSON file:");
+          if (filename) {
+            downloadLink.download = `${filename}.json`;
+            downloadLink.click();
+          } else {
+            alert("Filename cannot be empty. Save cancelled.");
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching the JSON file:", error);
+      });
+
+    // Saving the game grid to include it map package
+    const copyMatrix = data;
+
+    // saving other miscellaneous data
+
+    const collisionGrid = {};
+
+    // Load player data, could include more stuff later on
+    const playerStatus = {
+      movementAllow: player.allowMovement,
+      visibilityAllow: player.show,
+      spawn: null, // TODO: Figure this out
+    };
+
+    // Filling out the save package (there's a better name but this one sounds cooler)
+    const saveData = {
+      matrix: copyMatrix,
+      collisionGrid: collisionGrid,
+      player: playerStatus,
+    };
+    _downloadJSON(saveData);
   },
 };
